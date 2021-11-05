82e27afec36fa834a2032c4b9c6f9b9006492e53
9iaashalaby <30384731+9iaashalaby@users.noreply.github.com>
2021-11-04 20:50:44 -0700

    [Add] Use Contact Card component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
+import React from "react";
+
 import './App.css';
+import ContactCard from "./components/ContactCard";
+import ToDoItem from "./components/ToDoItem";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="contact">
+            <ContactCard
+                contact={{
+                    name: "Mr. Whiskerson",
+                    imgUrl: "http://placekitten.com/300/200",
+                    phone: "(212) 555-1234",
+                    email: "mr.whiskaz@catnap.meow"
+                }}
+            />
+
+            <ContactCard
+                contact={{
+                    name: "Fluffykins",
+                    imgUrl: "http://placekitten.com/400/200",
+                    phone: "(212) 555-2345",
+                    email: "fluff@me.com"
+                }}
+
+            />
+
+            <ContactCard
+                contact={{
+                    name: "Destroyer",
+                    imgUrl: "http://placekitten.com/400/300",
+                    phone: "(212) 555-3456",
+                    email: "ofworlds@yahoo.com"
+                }}
+            />
+
+            <ContactCard
+                contact={{
+                    name: "Felix",
+                    imgUrl: "http://placekitten.com/200/100",
+                    phone: "(212) 555-4567",
+                    email: "thecat@hotmail.com"
+                }}
+            />
+        </div>
+        // <div className="todo-list">
+        //     <ToDoItem />
+        //     <ToDoItem />
+        //     <ToDoItem />
+        //     <ToDoItem />
+        // </div>
+    );
 }
 
 export default App;
